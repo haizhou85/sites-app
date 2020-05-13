44664de9a32e947b7604915be23d60a062e249b3
haizhou85 <haizhou.guo@gmail.com>
2020-05-13 03:30:41 -0700

    created site controller methods

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,11 +1,52 @@
 class SitesController < ApplicationController
     def index
         @sites = Site.all
-        render json: @sites
+        if params[:zipcode].blank?
+            render json: @sites
+        else
+            render json: @sites.where(zipcode: params[:zipcode]) 
+        end
     end
 
     def show
-        @site = Site.find([params[:id]])
+        resource
         render json: @site
     end
+
+    def create
+        @site = Site.new(site_params)
+        if @site.save
+            render json: @site
+        else
+            render json: @site.errors
+        end
+    end
+
+    def destroy
+        resource
+        if @site.destroy
+            render json: @site
+        else
+            render json: @site.errors
+        end
+    end
+
+    def update
+        resource
+        if @site.update_attributes(site_params)
+            render json: @site
+        else
+            render json: @site.errors
+        end
+    end
+
+    private
+
+    def resource
+        @site = Site.find(params[:id])
+    end
+
+    def site_params
+        params.require(:site).permit(:name, :address, :state, :zipcode, :city)
+    end    
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,17 @@
 class Site < ApplicationRecord
     has_many :site_uses
+
+    def total_sizes
+        site_uses.sum(:size_sqft)
+    end
+
+    def primary_type
+        if site_uses.any?
+            UseType.find(site_uses.group(:use_type_id).order('sum_size_sqft DESC').sum(:size_sqft).first.first)
+        end            
+    end
+
+    def as_json(options = {})
+        super options.merge(methods: [:total_sizes, :primary_type])
+    end
 end
